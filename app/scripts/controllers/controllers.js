36c939e2d8a444cd6a0536276b3a8d42a8522e2c
masatoseki <masatoseki@the-neighbourhood.com>
2015-11-18 20:27:27 +0000

    Adding latest files

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -2,8 +2,25 @@
 
 var studioDbApp = angular.module('studioDBApp', []);
 
-studioDbApp.controller('StudioListCtrl', function ($scope) {
+studioDbApp.controller('StudioListCtrl', ['$scope', function ($scope) {
     $scope.filters = {};
+    $scope.friends = [
+        {name:'John', phone:'555-1212', age:10},
+        {name:'Mary', phone:'555-9876', age:19},
+        {name:'Mike', phone:'555-4321', age:21},
+        {name:'Adam', phone:'555-5678', age:35},
+        {name:'Julie', phone:'555-8765', age:29}
+    ];
+
+    $scope.predicate = 'age';
+    $scope.reverse = true;
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+    };
+
+    $scope.orderProp = 'name';
+
     $scope.studios = [
         {
             'name': 'ustwo',
@@ -25,5 +42,4 @@ studioDbApp.controller('StudioListCtrl', function ($scope) {
         }
     ];
 
-    $scope.orderProp = 'name';
-});
+}]);
